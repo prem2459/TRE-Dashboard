b8380997964a055ff93d91b5236828f51d5a1ffe
prem2459 <premkumar2459@gmail.com>
2023-03-30 20:56:22 +0530

    Project Setup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './Assets/Styles/index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { AuthLogin } from './AuthComponents/AuthLogin';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <AuthLogin>
     <App />
-  </React.StrictMode>,
+  </AuthLogin>,
 )
